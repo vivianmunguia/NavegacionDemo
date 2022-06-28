703853a7ccc5d6cda2cbddf73301a52e82fa58ae
Vivian Munguía <andy_munguia@hotmail.com>
2022-06-27 20:28:31 -0700

    The navigation between two screens was created

---
+++ NavegacionDemo/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Xamarin.Forms;
+
+namespace NavegacionDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            btnToSecondPage.Clicked += (sender, e) =>
+            {
+                Navigation.PushAsync(new SecondPage());
+            };
+        }
+    }
+}
